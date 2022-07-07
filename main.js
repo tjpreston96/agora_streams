d70d684c6045414323d2917a626c52bd525b161a
Tyler Preston <tpreston96@gmail.com>
2022-07-06 22:50:19 -0500

    handleUserJoined

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let remoteUsers = {};
 
 // Toggles local user to join a stream with camera and audio track.
 let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserJoined);
+
   let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
 
   localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
@@ -33,6 +35,32 @@ let joinStream = async () => {
 
   document.getElementById("join-btn").style.display = "none";
   document.getElementById("stream-controls").style.display = "flex";
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
+
+    player = `<div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                </div>`;
+
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
+
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
 };
 
 document.getElementById("join-btn").addEventListener("click", joinStream);
