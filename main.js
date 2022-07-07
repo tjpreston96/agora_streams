0ce3b1116b2d56e9448fa6afd22349df1c63a9b7
Tyler Preston <tpreston96@gmail.com>
2022-07-06 20:53:57 -0500

    joinStream

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const CHANNEL = process.env.CHANNEL;
 
 const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
+// Local video[0] and audio[1]
 let localTracks = [];
+// Other Users
 let remoteUsers = {};
 
+// Toggles local user to join a stream with camera and audio track.
 let joinAndDisplayLocalStream = async () => {
   let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
 
@@ -24,6 +27,11 @@ let joinAndDisplayLocalStream = async () => {
 
   await client.publish([localTracks[0], localTracks[1]]);
 
-  
-};
+  let joinStream = async () => {
+    await joinAndDisplayLocalStream();
+    document.getElementById("join-btn").style.display = "none";
+    document.getElementById("stream-controls").style.display = "flex";
+  };
 
+  document.getElementById("join-btn").addEventListener("click", joinStream);
+};
